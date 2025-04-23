c5fb49758c69fbdcbe76a589f630d18efed6371b
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2025-04-24 00:42:00 +0300

    feat(auth): add global auth module with service structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { OrderModule } from './order/order.module';
 import { PaymentModule } from './payment/payment.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, OrderModule, PaymentModule],
+  imports: [UsersModule, ProductsModule, OrderModule, PaymentModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
