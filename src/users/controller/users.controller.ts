4859a7859dcc2bf0a2e9fe4bd6ba4ea543f6f06c
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2025-04-24 11:32:37 +0300

    feat(users): add GET /users endpoint with pagination, sorting, and default query handling

--- src/users/controller/users.controller.ts
+++ src/users/controller/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { UsersService } from '../service/users.service';
+import { UserType } from '../utils/types';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Get()
+    getUsers(
+      @Query('page') page?: number,
+      @Query('limit') limit?: number,
+      @Query('sort') sort?: keyof UserType,
+      @Query('order') order?: 'asc' | 'desc',
+    ): UserType[] {
+      return this.usersService.findAll({ page, limit, sort, order });
+    }
+  }
--- src/users/service/users.service.ts
+++ src/users/service/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { UserType } from '../utils/types';
+import { dummyUsers } from 'src/data/DummyData';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    private users: UserType[] = dummyUsers;
+
+    findAll(query: {
+        page?: number;
+        limit?: number;
+        sort?: keyof UserType;
+        order?: 'asc' | 'desc';
+    }): UserType[] {
+        const { page = 1, limit = 10, sort = 'id', order = 'asc' } = query;
+
+        const sorted = [...this.users].sort((a, b) => {
+            if (order === 'asc') return a[sort] > b[sort] ? 1 : -1;
+            else return a[sort] < b[sort] ? 1 : -1;
+        });
+
+        const start = (page - 1) * limit;
+        const end = start + limit;
+        return sorted.slice(start, end);
+    }
+}
