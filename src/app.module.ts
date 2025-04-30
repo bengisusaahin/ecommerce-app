6956059d774aeaa20bb67047619c43a0c9f19b1c
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2025-04-30 18:06:37 +0300

    feat(database): setup dynamic TypeORM connection using env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { OrderModule } from './order/order.module';
 import { PaymentModule } from './payment/payment.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, ProductsModule, OrderModule, PaymentModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('POSTGRES_HOST'),
+        port: config.get<number>('POSTGRES_PORT'),
+        username: config.get<string>('POSTGRES_USER'),
+        password: config.get<string>('POSTGRES_PASSWORD'),
+        database: config.get<string>('POSTGRES_NAME'),
+        autoLoadEntities: config.get<string>('NODE_ENV') === 'development',
+        synchronize: config.get<string>('NODE_ENV') === 'development',
+        logging: config.get<string>('NODE_ENV') === 'development',
+        entities: [__dirname + '/**/entities/*.entity.{ts,js}'],
+      }),
+    }),
+    UsersModule, ProductsModule, OrderModule, PaymentModule, AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
