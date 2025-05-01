c8f277ca174006718bf780b33d2de77fdf175a93
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2025-05-01 18:22:29 +0300

    feat(order): add order service and controller to apply crud operations

--- src/order/controller/order.controller.ts
+++ src/order/controller/order.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, Query, UseGuards } from '@nestjs/common';
+import { OrderService } from '../service/order.service';
+import { CreateOrderDto } from '../dto/create-order.dto';
+import { Order } from '../entity/order.entity';
+import { UpdateOrderDto } from '../dto/update-order.dto';
 
-@Controller('order')
-export class OrderController {}
+@Controller('orders')
+export class OrderController {
+    constructor(private readonly ordersService: OrderService) {}
+
+    @Get() 
+    getOrders(
+        @Query('page') page?: number,
+        @Query('limit') limit?: number,
+        @Query('sort') sort?: string,
+        @Query('order') order?: 'asc' | 'desc',
+    ): Promise<Order[]> {
+        return this.ordersService.findAll({ page, limit, sort, order });
+    }
+
+    @Get(':id')
+    getOrderById(@Param('id', ParseIntPipe) id: number): Promise<Order> {
+        return this.ordersService.findOne(id);
+    }
+
+    @Post()
+    createOrder(@Body() createOrderDto: CreateOrderDto): Promise<Order> {
+        return this.ordersService.create(createOrderDto);
+    }
+
+    @Put(':id')
+    updateOrder(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() updateOrderDto: UpdateOrderDto,  
+    ): Promise<Order> {
+        return this.ordersService.update(id, updateOrderDto);  
+    }
+
+    @Delete(':id')
+    deleteOrder(@Param('id', ParseIntPipe) id: number): Promise<{ message: string }> {
+        return this.ordersService.remove(id);
+    }
+}
--- src/order/service/order.service.ts
+++ src/order/service/order.service.ts
@@ -1,4 +1,103 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from '../entity/order.entity';
+import { Repository } from 'typeorm';
+import { OrderItem } from '../entity/order-item.entity';
+import { CreateOrderDto } from '../dto/create-order.dto';
+import { User } from 'src/users/entity/user.entity';
+import { Product } from 'src/products/entity/product.entity';
+import { UpdateOrderDto } from '../dto/update-order.dto';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+    constructor(
+        @InjectRepository(Order)
+        private readonly orderRepository: Repository<Order>,
+
+        @InjectRepository(OrderItem)
+        private readonly orderItemRepository: Repository<OrderItem>,
+
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+
+        @InjectRepository(Product)
+        private productRepository: Repository<Product>,
+    ) { }
+
+    async findAll(query: { page?: number, limit?: number, sort?: string, order?: 'asc' | 'desc' }): Promise<Order[]> {
+        const { page = 1, limit = 10, sort = 'id', order = 'asc' } = query;
+
+        const [orders, total] = await this.orderRepository.findAndCount({
+            order: { [sort]: order.toUpperCase() as 'ASC' | 'DESC' },
+            skip: (page - 1) * limit,
+            take: limit,
+            relations: ['orderItems', 'orderItems.product', 'user'],
+        });
+
+        return orders;
+    }
+
+    async findOne(id: number): Promise<Order> {
+        const order = await this.orderRepository.findOne({
+            where: { id },
+            relations: ['orderItems', 'orderItems.product', 'user'],
+        });
+
+        if (!order) {
+            throw new NotFoundException(`Order with ID ${id} not found`);
+        }
+
+        return order;
+    }
+
+    async create(createOrderDto: CreateOrderDto): Promise<Order> {
+        const user = await this.userRepository.findOne({ where: { id: createOrderDto.userId } });
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+
+        const order = this.orderRepository.create({
+            user,
+            totalPrice: createOrderDto.totalPrice,
+        });
+        const savedOrder = await this.orderRepository.save(order);
+
+        const orderItems = await Promise.all(
+            createOrderDto.orderItems.map(async (item) => {
+                const product = await this.productRepository.findOne({ where: { id: item.productId } });
+                if (!product) {
+                    throw new NotFoundException(`Product with ID ${item.productId} not found`);
+                }
+
+                const orderItem = this.orderItemRepository.create({
+                    order: savedOrder,
+                    product,
+                    quantity: item.quantity,
+                    price: item.unitPrice,
+                    totalPrice: item.totalPrice,
+                });
+
+                return this.orderItemRepository.save(orderItem);
+            }),
+        );
+
+        savedOrder.orderItems = orderItems;
+        return savedOrder;
+    }
+
+    async update(id: number, updateOrderDto: UpdateOrderDto): Promise<Order> {
+        const order = await this.findOne(id);
+        Object.assign(order, updateOrderDto);
+        return this.orderRepository.save(order);
+    }
+
+
+    async remove(id: number): Promise<{ message: string }> {
+        const result = await this.orderRepository.delete(id);
+        if (result.affected === 0) {
+            throw new NotFoundException(`Order with ID ${id} not found`);
+        }
+
+        return { message: `Order with ID ${id} deleted successfully` };
+    }
+}
