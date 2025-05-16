cdb6d9407185bd4bf29ad934dcefc48a1c64ed53
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2025-05-16 18:09:03 +0300

    feat(orders-microservice): configure orders.module, app.module and main.ts to enable TCP microservice communication and database integration

--- orders-microservice/src/app.module.ts
+++ orders-microservice/src/app.module.ts
@@ -1,11 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './orders/entities/order.entity';
+import { OrderItem } from './orders/entities/order-item.entity';
 
 @Module({
-  imports: [OrdersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    OrdersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('ORDERS_POSTGRES_HOST'),
+        port: configService.get<number>('ORDERS_POSTGRES_PORT'),
+        username: configService.get<string>('ORDERS_POSTGRES_USER'),
+        password: configService.get<string>('ORDERS_POSTGRES_PASSWORD'),
+        database: configService.get<string>('ORDERS_POSTGRES_DB'),
+        autoLoadEntities: true,
+        entities: [Order, OrderItem],
+        synchronize: true,
+        cache: {
+          duration: configService.get<number>('TYPEORM_CACHE_DURATION'),
+        },
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
