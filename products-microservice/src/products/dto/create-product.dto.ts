e4e13c6258c69a26b23257cc98acb214eb5e09ae
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2025-05-16 11:31:06 +0300

    feat(products-microservice): transform api-gateway product dtos to products-mc

--- products-microservice/src/products/dto/create-product.dto.ts
+++ products-microservice/src/products/dto/create-product.dto.ts
@@ -1,1 +1,64 @@
-export class CreateProductDto {}
+import {
+    IsNotEmpty,
+    IsString,
+    IsNumber,
+    IsArray,
+    ValidateNested,
+    IsPositive,
+    Min,
+    IsOptional,
+    IsBoolean,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+class ProductImageDto {
+    @IsString()
+    url: string;
+
+    @IsNumber()
+    index: number;
+}
+
+export class CreateProductDto {
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @IsNotEmpty()
+    @IsString()
+    description: string;
+
+    @IsNumber()
+    @IsPositive()
+    @Min(0)
+    price: number;
+
+    @IsNumber()
+    @Min(0)
+    stock: number;
+
+    @IsOptional()
+    @IsBoolean()
+    is_active?: boolean;
+
+    @IsNotEmpty()
+    @IsNumber()
+    seller_id: number;
+
+    @IsOptional()
+    @IsNumber()
+    category_id?: number;
+
+    @IsOptional()
+    @IsNumber()
+    rating?: number;
+
+    @IsOptional()
+    @IsNumber()
+    sell_count?: number;
+
+    @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => ProductImageDto)
+    images: ProductImageDto[];
+}
